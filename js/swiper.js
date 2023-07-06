13d4bccafaf190ccc7c8f4a500774bf4b9108bf1
Viktor <121763231+ViktorMalkin@users.noreply.github.com>
2023-07-06 15:15:04 +0200

    Create swiper.js

---
+++ js/swiper.js
@@ -0,0 +1,49 @@
+/*
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'horizontall',/*'vertical',*/
+	//loop: true,
+	
+	// If we need pagination
+	/*
+	pagination: {
+		el: '.swiper-pagination',
+	},
+	*/
+	// Navigation arrows
+	
+	/*navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},*/
+	
+	// And if we need scrollbar
+	/*
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},*/
+	//});
+	
+
+//new Swiper ('.swiper', {
+	//direction: 'vertical',
+	//loop: true,
+//});	
+	
+	//navigation: {
+		//nextEl: '.swiper-button-next',
+		//prevEl: '.swiper-button-prev',
+	//},
+	
+	//pagination: {
+		//el: '.swiper-pagination',
+		//clickable: true,
+		//dynamicBullets: true,
+	//},
+	//scrollbar: {
+		//el: '.swiper-scrollbar',
+		//draggable: true,
+	//},
+//});
+
+//new Swiper ('.swiper');
